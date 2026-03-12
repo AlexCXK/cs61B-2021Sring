0b88e64cc271c1c12dea1bd5c03857daaec105fc
AlexCXK <changing199112@gmail.com>
2026-03-12 16:55:16 +0800

    bug lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -9,13 +9,21 @@ public class Collatz {
      * return the next number
      */
     public static int nextNumber(int n) {
-        if (n == 1) {
+//        if (n == 1) {
+//            return 1;
+//        } else if (n % 2 == 0) {
+//            return n / 2;
+//        } else {
+//            return 3 * n + 1;
+//        }
+        if (n  == 128) {
             return 1;
-        } else if (n % 2 == 0) {
-            return n / 2;
-        } else {
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
+
     }
 
     public static void main(String[] args) {
