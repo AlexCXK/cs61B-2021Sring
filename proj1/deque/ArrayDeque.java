2e774763306cab7e2352e0e93e11a21d1c0f24c2
AlexCXK <changing199112@gmail.com>
2026-03-16 23:48:09 +0800

    working on arraydeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -5,24 +5,44 @@ import java.util.Iterator;
 public class ArrayDeque<T> implements Deque<T>{
     private T[] items;
     private int size;
+    private int capacity;
+    private int nextFirstIndex;
+    private int nextLastIndex;
+    public ArrayDeque() {
+        capacity = 8;
+        items = (T[]) new Object[capacity];
+        size = 0;
+        nextFirstIndex = 4;
+        nextLastIndex = 5;
+
+    }
     @Override
     public void addFirst(T item) {
-
+        if(isFull()){
+            resize();
+        }
+        items[nextFirstIndex] = item;
+        nextFirstIndex = (nextFirstIndex - 1 +capacity) %capacity;
     }
 
     @Override
     public void addLast(T item) {
+        if(isFull()){
+            resize();
+        }
+        items[nextLastIndex] = item;
+        nextLastIndex = (nextFirstIndex + 1 +capacity) %capacity;;
 
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
@@ -42,11 +62,20 @@ public class ArrayDeque<T> implements Deque<T>{
 
     @Override
     public T get(int index) {
-        return null;
+        int physicalIndex = (nextFirstIndex  - 1 + index) % capacity;
+        return items[physicalIndex];
     }
 
     @Override
     public Iterator<T> iterator() {
         return null;
+    }
+
+    public  boolean isFull(){
+        return this.nextFirstIndex == this.nextLastIndex;
+    }
+
+    public  void resize() {
+
     }
 }
